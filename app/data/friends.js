a494f633301dfd1611b5ed05599deb3ace82bff4
Coach-BWise <wisebry11@gmail.com>
2020-04-20 16:58:21 -0400

    working app, needs input verification and images

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,16 @@
 let friendData = [
+  { name: "Ahmed", scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1] },
   {
-    name: "Ahmed",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1],
+    name: "Jessica Jimenez",
+    scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+  },
+  {
+    name: "Bryan W",
+    scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+  },
+  {
+    name: "Bob O.",
+    scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
   },
 ];
 
