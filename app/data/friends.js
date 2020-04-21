e5e64af530b7b5340815d9e5503a28f073feb8c9
Coach-BWise <wisebry11@gmail.com>
2020-04-21 13:30:41 -0400

    project complete verification added and photos added

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,16 +1,27 @@
 let friendData = [
-  { name: "Ahmed", scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1] },
   {
-    name: "Jessica Jimenez",
-    scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+    name: "Ahmed A. (example)",
+    photo:
+      "https://www.mopo.de/image/28086378/4x3/620/465/95a5d6066eec2db0116af599b9d2a6da/cX/img-1297.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1],
   },
   {
-    name: "Bryan W",
-    scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+    name: "Mary S. (example)",
+    photo:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSv1M7ePPv1XbP8u4ZIdIX0EQR4f0CejVuccQVK6dWUB1YZgksP&usqp=CAU",
+    scores: [1, 4, 5, 5, 3, 3, 3, 1, 2, 4],
   },
   {
-    name: "Bob O.",
-    scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+    name: "Samson D. (example)",
+    photo:
+      "https://ofad.org/files/daily-photo/recent-portraits-random-people-in-random-places_11.jpg",
+    scores: [3, 1, 2, 5, 4, 5, 3, 4, 1, 4],
+  },
+  {
+    name: "Bob O. (example)",
+    photo:
+      "https://i.dailymail.co.uk/i/pix/2013/08/29/article-2405475-1B8389EE000005DC-718_634x550.jpg",
+    scores: [4, 3, 4, 1, 1, 1, 1, 2, 2, 4],
   },
 ];
 
